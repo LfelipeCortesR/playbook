bde9e850ccf3b1f5c0dbc8a329cbf0cb032b92a8
LfelipeCortesR <lfelipecr76@hotmail.com>
2022-04-12 18:29:20 -0500

    Enter

---
+++ weekly_mission_2/examples0/example0-2.js
@@ -0,0 +1,7 @@
+// Ejemplo 2: Crear un objeto con propiedades
+const myObjetc2 = {
+    name: "Felipe",
+    age: 23
+  }
+  console.log("Ejemplo 2: Crear un objeto con propiedades")
+  console.log(myObjetc2)

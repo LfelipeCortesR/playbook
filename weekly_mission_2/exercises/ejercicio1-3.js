cb37be08beaa189f807573f22f33ad675d229519
LfelipeCortesR <lfelipecr76@hotmail.com>
2022-04-15 16:31:50 -0500

    Enter

---
+++ weekly_mission_2/exercises/ejercicio1-3.js
@@ -0,0 +1,93 @@
+const GitHub={
+    recentRepositories:"",
+    recentActivity:"",
+    yourTeams:"",
+    following:"",
+    forYou:"",
+}
+
+const repo={
+    name:"",
+    description:"",
+    date:"",
+    about:"",
+}
+
+const issue={
+    symbol:"",
+    name:"",
+    sending:"",
+    status:"",
+    description:"",
+    date:"",
+}
+
+const pull_request={
+    symbol:"",
+    name:"",
+    sending:"",
+    status:"",
+    description:"",
+    date:"",
+}
+
+const twitter={
+    Imgen:"",
+    twittear:"",
+    imagen_del_Segido:"",
+    nombre_del_Segido:"",
+    fecha:"",
+    hora:"",
+    link:"",
+    que_esta_pasando:""
+}
+
+const user = {
+    user: "felipe", 
+    username: "f_de_felipe", 
+    bio: "...",
+    age: 23
+}
+
+const trending_topic={
+    imagen_principal:"",
+    nombre:"",
+    tendencia_sobre:"",
+    fecha:"",
+    numero_tweets:"",
+}
+
+const hashtag={
+    nombre:"",
+    info:"",
+    arroba:"",
+    fecha:"",
+    descripcion:"",
+    imagen:"",
+    hashtag:""
+}
+
+const uber={
+
+}
+
+const profile={
+    Foto:"",
+    Nombre:"",
+    n_Viajes: 77,
+    calificacion:"5 estrellas",
+    habla:"español",
+    de:"puebla",
+    preguntas:""
+}
+
+const travel={
+    salida:"",
+    destino:"",
+    ruta:"",
+    hora_salida:"",
+    hora_llegada:"",
+    tipo_de_Viaje:"",
+    costo:"",
+    tipo_de_pago:""
+}
